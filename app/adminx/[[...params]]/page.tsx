f8dc77c76a5247619db61e38d067a4458e2af10a
nguyenphuhao <nguyenphuhao.it@gmail.com>
2026-04-12 16:34:37 +0700

    refactor: remove Keystatic admin UI, replaced by Composer

--- app/adminx/[[...params]]/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import { makePage } from "@keystatic/next/ui/app";
-import keystaticConfig from "@/keystatic.config";
-
-// Keystatic's JS hardcodes /keystatic in all router.push() calls.
-// We serve the studio at both /adminx (entry point) and /keystatic (internal navigation).
-export default makePage(keystaticConfig);
